c95894fe7cebaa30ee3b7d08a84eb050bae511f5
Harry-Findlay <harry.findlay2@gmail.com>
2026-04-28 02:13:35 +0100

    V1.1.17 - Fix for trafficlights in macOS

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
 
+// Add platform to root element so CSS can target it
+document.documentElement.dataset.platform = navigator.userAgent.includes('Mac') ? 'mac' : 'other'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
